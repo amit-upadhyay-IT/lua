b778a8f98d5acd8c5535c8cb4e14b07c4320145f
Amit Upadhyay <youarefreak1@gmail.com>
2017-06-14 19:24:20 +0530

    Update gethashcontent.lua

--- gethashcontent.lua
+++ gethashcontent.lua
@@ -1,25 +1,33 @@
---[[
-
---]]
 
 local noOfArgs = KEYS[1] -- storing the number of arguments that will get passed from cli
 
-local varOne = ARGV[1]
-local varTwo = ARGV[1]
+--[[
+	run a loop noOfArgs time and initialize table elements, since we don't know the number of sets to be intersected so we will use Table (arrays)
+--]]
 
-local setNameOne = 'points:'..varOne -- first set
-local setNameTwo = 'points:'..varTwo -- second set
+local setsTable = {}
+
+-- coppies all the set names in setsTable
+for i = 0, noOfArgs, 1 do
+	setsTable[i] = 'points:'..ARGV[i]
+end
+
+
+-- now find intersection
+local intersectedVal = redis.call("sinter", setsTable[0], setsTable[1]) -- finding first intersection because atleast we will have two sets
+
+for i = 2, noOfArgs, 1 do
+	intersectedVal = redis.call("sinter", intersectedVal, setsTable[i])
+end
 
 local allString = ""
 
-local name=redis.call("sinter", setNameOne, setNameTwo, ARGV[2])
+-- now val contains all intersected value
 
-
-for _,key in ipairs(name) do
+for _,key in ipairs(intersectedVal) do
 	local val = redis.call("HGET", key, "name")
-	io.write(val)
+	--io.write(val)
 	allString = val..', '..allString
 end
 
---return allString
-return KEYS.length
+return allString
